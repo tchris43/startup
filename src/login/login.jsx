aa0433097d9b992f9055c02122217a6a832a71f4
Taylor Christensen <tchris43@byu.edu>
2026-02-12 22:04:28 -0700

    added main for login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main className="container-fluid text-center bg-light text-dark">
+        <form method="get" action="search.html">
+            <h1>Welcome to NextEvent</h1>
+            <div className="input-group">
+                <span className = "input-group-text"for = "username">Username:</span>
+                <input className="form-control" type = "text" placeholder="your name"/>
+            </div>
+            <div className="input-group">
+                <span className = "input-group-text" for = "password">Password:</span>
+                <input className = "form-control" type = "password" placeholder="your password"/>
+            </div>
+            <button type="submit" className = "btn btn-primary">Login</button>
+            <button type="submit" className = "btn btn-secondary">Create</button>
+        </form>
     </main>
   );
 }
