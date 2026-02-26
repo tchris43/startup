dc151b08e12a4e0629ee6f4f2b43366e3c52ac79
Taylor Christensen <tchris43@byu.edu>
2026-02-25 21:08:02 -0700

    added placeholder for api calls

--- src/discover/discover.jsx
+++ src/discover/discover.jsx
@@ -1,119 +1,3 @@
-// import React from 'react';
-// import './discover.css';
-
-
-
-// export function Discover({ user }) {
-//     const [count, setCount] = React.useState(parseInt(localStorage.getItem('count')) || 0);
-//     const [msg, setMsg] = React.useState('...listening');
-
-//     React.useEffect(() => {
-//         setInterval(() => {
-//             const names = ['bob', 'fish', 'tim'];
-//             const randomName = names[Math.floor(Math.random() * names.length)];
-//             const randomCount = Math.floor(Math.random() * 100) + 1;
-//             const newMsg = `${randomName}: ${randomCount}`;
-//             setMsg(newMsg);
-//         }, 1000);
-//     })
-
-//     function sendInvite() {
-//         setCount(count + 1);
-//         localStorage.setItem('count', count + 1);
-//     }
-
-//     return (
-//         <main className="bg-light text-dark">
-//             <h2>
-//                 Your Upcoming Events - {user}
-//             </h2>
-//             <div id="upcomingCarousel" className="carousel slide">
-//                 <div id="slide1" className="upcoming carousel-item">
-//                     <span className="card left-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 1</figcaption>
-//                     </span>
-//                     <span className="card middle-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 2</figcaption>
-//                     </span>
-//                     <span className="card right-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 3</figcaption>
-//                     </span>
-//                 </div>
-//                 <div id="slide2" className="upcoming carousel-item">
-//                     <span className="card left-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 4</figcaption>
-//                     </span>
-//                     <span className="card middle-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 5</figcaption>
-//                     </span>
-//                     <span className="card right-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 6</figcaption>
-//                     </span>
-//                 </div>
-//                 <a className="carousel-control-prev" data-slide="prev">
-//                     <span className="carousel-control-prev-icon"></span>
-//                 </a>
-//                 <a className="carousel-control-next" data-slide="next">
-//                     <span className="carousel-control-next-icon"></span>
-//                 </a>
-//             </div>
-//             <br />
-//             <h2>
-//                 Browse Events
-//             </h2>
-//             <div id="eventCarousel" className="carousel slide">
-//                 <div id="slide1" className="upcoming carousel-item">
-//                     <span className="card left-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 1</figcaption>
-//                     </span>
-//                     <span className="card middle-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 2</figcaption>
-//                     </span>
-//                     <span className="card right-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 3</figcaption>
-//                     </span>
-//                 </div>
-//                 <div id="slide2" className="upcoming carousel-item">
-//                     <span className="card left-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 4</figcaption>
-//                     </span>
-//                     <span className="card middle-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 5</figcaption>
-//                     </span>
-//                     <span className="card right-card">
-//                         <img src="card.png" width={200} />
-//                         <figcaption>Event 6</figcaption>
-//                     </span>
-//                 </div>
-//                 <a className="carousel-control-prev" data-slide="prev">
-//                     <span className="carousel-control-prev-icon"></span>
-//                 </a>
-//                 <a className="carousel-control-next" data-slide="next">
-//                     <span className="carousel-control-next-icon"></span>
-//                 </a>
-//             </div>
-
-//             <label for="invites">Invites:</label>
-//             <textarea id="invites" name="invites">Send and Recieve Invites!</textarea>
-//             <div> {msg} </div>
-//             <button onClick={sendInvite}>Send!</button>
-//             <div>{count}</div>
-
-//         </main>
-//     );
-// }
-
 import React from 'react';
 import './discover.css';
 
@@ -133,63 +17,67 @@ export function Discover({ user }) {
         }, 5000);
     }, [])
 
-    function sendInvite() {
-        
-    }
 
     const [slide, setSlide] = React.useState(0);
     const [slide2, setSlide2] = React.useState(0);
     const [pop, setPop] = React.useState(null);
     const [invite, setInvite] = React.useState(null);
 
+    const [events, setEvents] = React.useState(null);
+
+    React.useEffect(() => {
+        setEvents("card.png");
+    }, [])    
+    
+
     return (
         <main className="bg-light text-dark">
             <h2>
-                Your Upcoming Events - {user}
+                Your Upcoming events - {user}
             </h2>
 
             <div className="carousel slide">
                 <div className={`upcoming carousel-item ${slide === 0? 'active' : ''}`}>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 1</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 2</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 3</figcaption>
                     </span>
                 </div>
 
                 <div className={`upcoming carousel-item ${slide === 1? 'active' : ''}`}>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 4</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 5</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 6</figcaption>
                     </span>
                 </div>
 
                 <div className={`upcoming carousel-item ${slide === 2? 'active' : ''}`}>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 7</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 8</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 9</figcaption>
                     </span>
                 </div>
@@ -204,56 +92,56 @@ export function Discover({ user }) {
 
 
             <h2>
-                Browse Events
+                Browse {events}
             </h2>
 
             <div className="carousel slide">
                 <div className={`upcoming carousel-item ${slide2 === 0? 'active' : ''}`}>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 1</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 2</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 3</figcaption>
                     </span>
                 </div>
 
                 <div className={`upcoming carousel-item ${slide2 === 1? 'active' : ''}`}>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 4</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 5</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 6</figcaption>
                     </span>
                 </div>
 
                 <div className={`upcoming carousel-item ${slide2 === 2? 'active' : ''}`}>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 7</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 8</figcaption>
                     </span>
                     <span className="card left-card">
-                        <img src="card.png" width={200} />
+                        <img src={events} width={200} />
                         <figcaption>Event 9</figcaption>
                     </span>
                 </div>
 
-                {pop && <div className="position-fixed top-0 end-0 alert alert-dark m-3">
+                {pop && <div className="position-fixed top-0 end-0 alert alert-info m-3">
                     {pop}
                 </div>}
 
