d93d84c12537cd924a2a529f94316f4ca391f789
Taylor Christensen <tchris43@byu.edu>
2026-03-13 17:28:12 -0600

    configured the router, memory, and middleware for service

--- service/index.js
+++ service/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+const authCookieName = "token";
+
+let users = [];
+let preferences = [];
+
+let apiRouter = express.Router();
+
 const port = process.argv.length > 2 ?
 process.argv[2] : 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(`/api`, apiRouter);
+
 
 app.get(/.*/, (_req, res) => {
     res.send({msg: "service message"});
