ac1962b3fde9b6ad9c0eedc63c64b6289e7710fc
Taylor Christensen <tchris43@byu.edu>
2026-04-02 17:29:09 -0600

    constructed the InviteEventClass

--- src/discover/eventNotifier.js
+++ src/discover/eventNotifier.js
@@ -10,3 +10,26 @@ class EventMessage {
         this.value = value;
     }
 }
+
+class InviteEventNotifier {
+    events = [];
+    handlers = [];
+
+    constructor() {
+        let port = window.location.port;
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+        this.socket.onopen = (event) => {
+            this.receiveEvent(new EventMessage('NextEvent', InviteEvent.System, {msg: 'connected'}));
+        };
+        this.socket.onclose = (event) => {
+            this.receiveEvent(new EventMessage('NextEvent', InviteEvent.System, {msg: 'disconnected'}));
+        };
+        this.socket.onmessage = async (msg) => {
+            try {
+                const event = JSON.parse(await msg.data.text());
+                this.receiveEvent(event);
+            } catch {}
+        };
+    }
+}
