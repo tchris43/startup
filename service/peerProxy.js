19500670ba42a7b912351a103102b9c7ed919c3b
Taylor Christensen <tchris43@byu.edu>
2026-04-01 21:40:05 -0600

    forwards messages

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -2,4 +2,16 @@ const { WebSocketServer, WebSocket } = require('ws');
 
 function peerProxy(httpServer){
     const socketServer = new WebSocketServer({server: httpServer});
+
+    socketServer.on('connection', (socket) => {
+        socket.isAlive = true;
+
+        socket.on('message', function message(data) {
+            socketServer.clients.forEach((client) => {
+                if (client !== socket && client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+        });
+    })
 }
